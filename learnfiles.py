9557bcc658df1d524a20eb12e1c0239e1ae83e21
nitin-yadav5939 <52251172+nitin-yadav5939@users.noreply.github.com>
2020-10-01 09:26:58 +0530

    Update learnfiles.py

--- learnfiles.py
+++ learnfiles.py
@@ -7,8 +7,9 @@ from datetime import datetime
 
 while True:
 
-    print("**Welcome to healthy Life System**")
+    print("**Welcome to Healthy Life System**")
     print("We will take care of your eyes,heart and body hydration\n")
+    
     time.sleep(15)
     mixer.init()
     mixer.music.load("water.mp3")
@@ -29,24 +30,27 @@ while True:
     mixer.music.play()
     time.sleep(10)
     mixer.music.stop()
-    value2 = input("Please update your done task\n")
+    value2 = input("Please update your done tasks\n")
     with open("Eyes_Exercise.txt", "a") as op:
         op.write(str([str(time.ctime())]) + ":" + value2 + "\n")
     print("successfully updated")
     time.sleep(15)
     mixer.init()
     mixer.music.load("physicalexercise.mp3")
-    print("Reminder!! Physical Exercise")
+    print("Reminder!! Physical Exercise Time")
     mixer.music.set_volume(20)
     mixer.music.play()
     time.sleep(10)
     mixer.music.stop()
+    
     print("Its time for little pysical exercise")
     value3 = input("Please write your done task\n")
     with open("Physical_Exercise.txt", "a") as op:
         op.write(str([str(time.ctime())]) + ":" + value3 + "\n")
     print("successfully updated")
+    
     a=int(input(" Press 1 for continuation and 0 to exit"))
+    
     if a==0:
         exit()
     else:
